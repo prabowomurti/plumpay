642e5fd12739ec32e245917c42451e45abeecba5
Prabowo Murti <prabowo.murti@gmail.com>
2017-03-11 19:39:52 +0700

    Remove password field from login form

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -21,8 +21,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                 <div style="color:#999;margin:1em 0">
