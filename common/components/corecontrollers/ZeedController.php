90e826396b59a6158e90c3277fe25060d04d58f3
Prabowo Murti <prabowo.murti@gmail.com>
2017-03-12 07:55:24 +0700

    Add core active record and controller

---
+++ common/components/corecontrollers/ZeedController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\components\corecontrollers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+/**
+ * Simple and sweet class for every controller
+ * 
+ * @author Prabowo Murti <prabowo.murti@gmail.com>
+ */
+
+class ZeedController extends Controller
+{
+}
